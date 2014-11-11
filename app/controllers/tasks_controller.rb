52635907756541cdceb868496f1f0145a2b58516
EtienneMangino <etiennemangino@gmail.com>
2014-11-11 14:32:52 +0100

    fin de task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,15 +9,23 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
   end
 
   def edit
+
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
   end
 
   def destroy
@@ -29,5 +37,10 @@ class TasksController < ApplicationController
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+
+    params.require(:task).permit(:name)
   end
 end
